822a31e01b525ef9ff7fdec9b7ec51ebeb5410d2
Pedro Henrique <henriquevic012@gmail.com>
2019-02-12 21:55:43 -0200

    Update app.js

--- app.js
+++ app.js
@@ -2,36 +2,82 @@
 let allEnemies = [];
 
 class Enemy{
-	constructor(){
-		this.sprite = 'images/enemy-bug.png';
-		this.x = -100;
-		this.y = 60;
-		this.speed = 2;
-	}
-	update(dt){
-		this.x += this.speed * 2;
-	}
-	render(){
-		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-	}
+    constructor(){
+        this.sprite = 'images/enemy-bug.png';
+        this.x = 0;
+        this.y = 60;
+        this.speed = 3;
+    }
+    update(dt){
+        this.x += this.speed * dt * 61;
+        if (this.x > 500){
+            this.x = -100;
+        }
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 }
 
-allEnemies = [new Enemy()];
+class EnemyTwo{
+    constructor(){
+        this.sprite = 'images/enemy-bug.png';
+        this.x = 0;
+        this.y = 143;
+        this.speed = 1;
+    }
+    update(dt){
+        this.x += this.speed * dt * 65;
+        if (this.x > 500){
+            this.x = -100;
+        }
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+class EnemyThree{
+    constructor(){
+        this.sprite = 'images/enemy-bug.png';
+        this.x = 0;
+        this.y = 226;
+        this.speed = 5;
+    }
+    update(dt){
+        this.x += this.speed * dt * 68;
+        if (this.x > 500){
+            this.x = -100;
+        }
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+allEnemies = [new Enemy(), new EnemyTwo(), new EnemyThree()];
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 class Player {
-  handleInput() {
-    // posição x, y e sprite do player
-  }
-  update() {
-    // caso queira animar ou atualizar
-  }
-  render() {
-    // renderizar no canvas com o método ctx.drawImage()
-  }
+    constructor(){
+        this.sprite = 'images/char-boy.png';
+        this.x = 202;
+        this.y = 404;
+        this.speed = 1.3;
+    }
+    handleInput(direct){
+    }    
+    update(dt){
+
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 }
+
+
 const player = new Player()
 
 
@@ -54,3 +100,7 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+
