427545621f02f64e87feb0ec0ae5386710d2ef1f
Pedro Henrique <henriquevic012@gmail.com>
2019-02-18 21:47:11 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-// Enemies our player must avoid
+//Os inimigos e o player serão instanciados aqui
 let allEnemies = [];
 
+//Uma classe que cria os inimigos
 class Enemy {
+    /*constroi as dimensões do inimigo, bem como sua largura e sua altura,
+    recebe três argumentos: dimenção de x, dimensão de y e a velocidade de acordo com a criação dos inimigos
+    na matriz definida acima*/
     constructor(posX, posY, speed) {
         this.sprite = 'images/enemy-bug.png';
         this.x = posX;
@@ -10,23 +14,27 @@ class Enemy {
         this.largura = 50;
         this.altura = 50;
     }
+    /*Aqui está presente o método de atualização dos inimigos onde seu SPEED será atualizado
+    Também faz com que os inimigos se movam no cenário, recebe o argumento (dt) passado pelo engine.js*/
     update(dt) {
         this.x += this.speed * 2;
         if (this.x > 500) {
             this.x = -100;
         }
     }
+    /*Os inimigos são desenhados um a um por meio das dimensões informadas na array allEnemies passadas ao
+    método ctx.drawImage dada pela engine.js*/
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
+//A criação de três objetos inimigos por meio da class Enemy
 allEnemies = [new Enemy(-100, 60, 2.5), new Enemy(-100, 143, 2), new Enemy(-100, 226, 3.5)];
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
+//A classe responsável por criar as dimensões, atualização, redenrização e animação do player;
 class Player {
+    //Aqui são criados as dimensões e é chamado também a imagem do player
     constructor() {
         this.sprite = 'images/char-boy.png';
         this.x = 202;
@@ -35,6 +43,11 @@ class Player {
         this.largura = 50;
         this.altura = 50;
     }
+    /*Método responsável de identificar o comando de direção para animar o player
+    de acordo com as instruções passadas pela função document.addEventListener
+    a handleInput  recebe um parâmetro direct que define a direção em que o player
+    será movido, se o usuário direcionar qualquer tecla direcional do teclado
+    o player deve responder habilmente a tal orientação*/
     handleInput(direct) {
         if (direct === 'right' && this.x < 390) {
             this.x += 101;
@@ -49,6 +62,9 @@ class Player {
             this.y -= 83;
         }
     }
+    /*A função colid é de suma importância para o objetivo e a lógica do jogo
+    ela detecta a colisão entre inimigos e o player, caso aconteça uma colizão
+    o inimigo volta altomaticamente para o seu local de origem*/
     colid(jogador, inimigos) {
         for (let c = 0; c < inimigos.length; c++) {
             if (jogador.x < inimigos[c].x + inimigos[c].largura &&
@@ -61,30 +77,26 @@ class Player {
             }
         }
     }
+    /*Aqui é possível identificar quando o player atigi a água, fazendo com que o player
+    volte ao seu local de origem*/
     update() {
         if (this.y <= 0) {
             this.x = 202;
             this.y = 404;
         }
+        //verificação de colisão a cada atualização 
         this.colid(player,allEnemies)
     }
+    //Desenha o player na tela, e de acordo com as suas coordenadas
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
-
+//Cria um objeto Player por meio da class Player
 const player = new Player()
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-
+/*Método responsável de colher as infomações quando o usuário preciona as teclas UP, DOWN, REIGHT ou LEFT
+e os passa para a função handleInput na classe PLayer.handleInput*/
 document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
